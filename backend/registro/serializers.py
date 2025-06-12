52a3fdd99cc830c728708c2c982dea0a67f85d9d
Luiz-021 <luizgute@outlook.com>
2025-06-11 23:40:05 -0300

    Parte de registro testada e funcionando

--- backend/registro/serializers.py
+++ backend/registro/serializers.py
@@ -8,23 +8,36 @@ class ParticipanteSerializer(serializers.ModelSerializer):
     class Meta: model = Participante; fields = '__all__'
 
 class PesoQuestaoSerializer(serializers.ModelSerializer):
-    class Meta: model = PesoQuestao; fields = '__all__'
+    class Meta:
+        model = PesoQuestao; fields = '__all__'
+
+# Serializer aninhado (para criar/atualizar provas)
+class PesoNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PesoQuestao
+        fields = ('numero_questao','peso')  
 
 class ProvaSerializer(serializers.ModelSerializer):
-    pesos = PesoQuestaoSerializer(many=True, required=False)
+    pesos = PesoNestedSerializer(many=True, required=False)
     class Meta:
         model = Prova
         fields = ['id','nome','data','numero_questoes','respostas_corretas','pesos']
-    def create(self, data):
-        pesos = data.pop('pesos',[])
-        p = Prova.objects.create(**data)
-        for x in pesos: PesoQuestao.objects.create(prova=p,**x)
-        return p
-    def update(self, inst, data):
-        pesos = data.pop('pesos',None)
-        for k,v in data.items(): setattr(inst,k,v)
-        inst.save()
-        if pesos is not None:
-            inst.pesos.all().delete()
-            for x in pesos: PesoQuestao.objects.create(prova=inst,**x)
-        return inst
+
+    def create(self, validated_data):
+        pesos_data = validated_data.pop('pesos', [])
+        prova = Prova.objects.create(**validated_data)
+        for peso in pesos_data:
+            PesoQuestao.objects.create(prova=prova, **peso)
+        return prova
+
+    def update(self, instance, validated_data):
+        pesos_data = validated_data.pop('pesos', None)
+        for attr, val in validated_data.items():
+            setattr(instance, attr, val)
+        instance.save()
+        if pesos_data is not None:
+            # apaga pesos antigos e recria
+            instance.pesos.all().delete()
+            for peso in pesos_data:
+                PesoQuestao.objects.create(prova=instance, **peso)
+        return instance
